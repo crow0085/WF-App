6f28a895934395b589594fa4f9b63bf31f4e376f
crow0085 <cr0w0085@algonquinlive.com>
2026-05-18 04:15:14 -0400

    added basic accordions for relic eras and their relics

--- src/pages/Relics/Relics.tsx
+++ src/pages/Relics/Relics.tsx
@@ -13,7 +13,7 @@ export interface Relic { // this is for the relic itself
   name: string;
   vaulted: boolean;
   era: string;
-  uniqueName: string; 
+  uniqueName: string;
   rewards: RelicReward[];
 }
 
@@ -25,7 +25,7 @@ interface EraGroups {
   Requiem: Relic[];
 }
 
-function mapRawToRelic(rawRelic: any){
+function mapRawToRelic(rawRelic: any) {
   const keywords = ["exceptional", "relic"];
 
   const name = rawRelic.name.split(' ').filter((word: string) => !keywords.includes(word.toLowerCase())).join(' ');
@@ -38,7 +38,7 @@ function mapRawToRelic(rawRelic: any){
     name: reward.item.name,
     ducats: 0,
     plat: 0
-  })); 
+  }));
 
 
   return {
@@ -50,7 +50,7 @@ function mapRawToRelic(rawRelic: any){
   }
 }
 
-function mapRelicsToEra(relics: Relic[]){
+function mapRelicsToEra(relics: Relic[]) {
   const Lith: Relic[] = relics.filter((relic: Relic) => {
     return relic.name.startsWith("Lith")
   })
@@ -79,7 +79,7 @@ function mapRelicsToEra(relics: Relic[]){
     Axi,
     Requiem
   }
-  
+
 }
 
 export default function Relics() {
@@ -89,7 +89,7 @@ export default function Relics() {
 
     // Inside a function, helper, or event handler:
     invoke('get_warframe_items', { category: 'Relics', forceFetch: false })
-      .then((data: any) => {     
+      .then((data: any) => {
         const mappedRelics = data.map(mapRawToRelic)
         const mappedByEra = mapRelicsToEra(mappedRelics);
         setRelics(mappedByEra);
@@ -99,15 +99,100 @@ export default function Relics() {
         console.error("Rust Backend Error:", error);
       });
 
-      if (relics != null)
-        console.log(relics)
+    if (relics != null)
+      console.log(relics)
 
     return () => console.log('cleanup');
   }, []);
 
   return (
-    <div className="container">
-      <h1 style={{ color: "#fff", fontSize: "2.25rem" }}>Relics page</h1>
-    </div>
+    <>
+      <div className="container">
+        <h1 style={{ color: "#fff", fontSize: "2.25rem" }}>Relics page</h1>
+      </div>
+      <div>
+        {
+          relics &&
+            (
+              Object.entries(relics).map( ([eraName, relics]) => (
+                <EraAccordiion
+                  eraName={eraName}
+                  relics={relics}
+                />
+              ))
+            )
+        }
+      </div>
+    </>
   );
+
+  interface EraAccordionProps {
+    eraName: string;
+    relics: Relic[];
+  }
+
+  function EraAccordiion(props: EraAccordionProps) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+      <>
+        <div>
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <span>{props.eraName}</span>
+            <span>{isOpen ? "▼" : "▶"}</span>
+          </button>
+
+          {
+            isOpen && (
+              <ul>
+                {
+                  props.relics.map( (relic: Relic) => (
+                    <RelicAccordion
+                      relic={relic}
+                    />
+                  ))
+                }
+              </ul>
+            )
+          }
+        </div>
+      </>
+    )
+  }
+
+
+  interface RelicAccordionProps{
+    relic: Relic
+  }
+
+  function RelicAccordion (props: RelicAccordionProps){
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+      <>
+        <div>
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <span>{props.relic.name}</span>
+            <span>{isOpen ? "▼" : "▶"}</span>
+          </button>
+
+          {
+            isOpen && (
+              <ul>
+                {
+                  props.relic.rewards.map( (reward: RelicReward) => (
+                    <li>
+                      <span>{reward.name}</span>
+                      <span>{reward.rarity}</span>
+                    </li>
+                  ))
+                }
+              </ul>
+            )
+          }
+        </div>
+      </>
+    )
+  }
 }
