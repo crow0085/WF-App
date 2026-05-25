339d81051b9f607330c26a8d35858ba9808c8c68
crow0085 <cr0w0085@algonquinlive.com>
2026-05-25 02:40:04 -0400

    creating setup for self contained python fastapi to be used for ocr

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,17 +4,17 @@ pub fn run() {
     #[cfg(target_os = "linux")]
     std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
 
-    let client = reqwest::Client::builder()
-        .user_agent("WF-APP (GitHub: crow0085)")
-        .build()
-        .expect("Failed to build global HTTP client");
+    // let client = reqwest::Client::builder()
+    //     .user_agent("WF-APP (GitHub: crow0085)")
+    //     .build()
+    //     .expect("Failed to build global HTTP client");
 
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_screenshots::init())
         .plugin(tauri_plugin_http::init())
-        .manage(client)
+        //.manage(client)
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
 
