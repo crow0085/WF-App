7b8b81a0fded30334056a55a3005f37342a4e8a2
crow0085 <crow0085@algonquinlive.com>
2026-06-06 01:33:03 -0400

    set to base app with just plain home page

--- src/pages/MasteryTracker/MasteryMelee.tsx
+++
@@ -1,7 +0,0 @@
-export default function MasteryMelee() {
-  return (
-    <div>
-      <h1 style={{ color: "#fff", fontSize: "2.25rem" }}>Melee</h1>
-    </div>
-  );
-}
--- src/pages/MasteryTracker/MasterySecondary.tsx
+++
@@ -1,7 +0,0 @@
-export default function MasterySecondary() {
-  return (
-    <div>
-      <h1 style={{ color: "#fff", fontSize: "2.25rem" }}>Secondary</h1>
-    </div>
-  );
-}
